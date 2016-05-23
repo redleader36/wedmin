0460ce4c78730a04c81f5d7b5a3ecf5bf463a8db
redleader36 <redleader36@gmail.com>
2016-05-23 16:38:06 -0500

    removed dj-database-url

--- wedmin/settings.py
+++ wedmin/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'wedmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
